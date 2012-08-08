433281de3cfadbee35c22b32cdc445d72c19d4ec
Kelly Mahan <kmahan@kmahan.com>
2012-08-08 11:13:44 -0500

    first commit for twilioauth gem. simple 1 message with returned pin working.

---
+++ lib/twilioauth.rb
@@ -0,0 +1,36 @@
+require "twilio-ruby"
+require 'active_support/secure_random'
+
+module Twilioauth
+  
+  class Connection
+    
+    attr_accessor :client, :account
+    
+    def initialize(account_sid, auth_token)
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      @account = @client.account
+    end
+    
+    def sms(message, to, from = default_number)
+      @client.account.sms.messages.create(
+        :from => from,
+        :to => to,
+        :body => message
+      )
+    end
+    
+    def default_number
+      #retrieve the default number
+      @default_number ||= @account.incoming_phone_numbers.list.first.phone_number
+    end
+  end
+  
+  def self.auth(number, options = {})
+    connection = Twilioauth::Connection.new(options[:account_sid], options[:auth_token])
+    newpin = ActiveSupport::SecureRandom.hex(3)
+    connection.sms(newpin, number)
+    return newpin
+  end
+  
+end
---
+++ twilioauth.gemspec
@@ -0,0 +1,26 @@
+lib = File.expand_path('../lib/', __FILE__)
+$:.unshift lib unless $:.include?(lib)
+
+Gem::Specification.new do |s|
+  s.name        = 'twilioauth'
+  s.version     = '0.0.1'
+  s.date        = '2012-08-08'
+  s.summary     = "2 factor auth for twilio"
+  s.description = "2 factor auth for twilio"
+  s.authors     = ["Kelly Mahan"]
+  s.email       = 'kmahan@kmahan.com'
+  s.files       = ["lib/twilioauth.rb"]
+  s.homepage    = 'http://rubygems.org/gems/twilioauth'
+  
+  s.files = `git ls-files`.split("\n")
+  #s.test_files = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.require_path = 'lib'
+  
+  s.requirements << "twilio-ruby"
+  s.requirements << "active_support"
+  
+  s.add_dependency "twilio-ruby"
+  s.add_dependency "active_support"
+  
+  
+end
